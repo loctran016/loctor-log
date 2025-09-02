0bbf71547f3844d08b403e2cb3ce6544e53c006f
Tran Tan Loc <locphuc016@gmail.com>
2025-09-03 00:58:48 +0700

    Update index.vue

--- app/pages/[year]/[subject]/index.vue
+++ app/pages/[year]/[subject]/index.vue
@@ -105,7 +105,7 @@ useSeoMeta({
 
     <!-- Main list of posts -->
     <TransitionGroup name="list" tag="ul" class="grid grid-cols-[repeat(auto-fit,minmax(24rem,1fr))] gap-4 justify-center lg:justify-start w-4/5 mx-auto mt-2 mb-10">
-      <li v-for="(post,index)  in queryPostsByTags" :data-index="index" :key="index" :class="isNotDefaultTagsOnly && 'min-h-[7rem] p-4'" class="group" >
+      <li v-for="(post,index)  in queryPostsByTags" :data-index="index" :key="index" :class="isNotDefaultTagsOnly && 'h-[7rem] p-4'" class="group" >
           <NuxtLink :to="post.path" class="px-4 flex flex-col gap-2  h-full rounded pb-2 bg-gray-100 dark:bg-neutral-900 dark:hover:bg-neutral-800 hover:bg-slate-200 transition-all duration-200 cursor-pointer pt-2 lg:py-3 relative">
               <h3 class="font-[Montserrat] dark:text-white font-semibold lg:text-lg flex-grow tracking-wide lg:tracking-wider max-w-[95%]">
                 {{ post.title }}
