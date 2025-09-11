8a5513d5209dda26e1b6e6d9cd085aba495fb1bc
Tran Tan Loc <locphuc016@gmail.com>
2025-09-11 14:34:15 +0700

    misc

--- app/components/content/Info.vue
+++ app/components/content/Info.vue
@@ -1,6 +1,6 @@
 <template>
-<section class="py-6 px-8 bg-emerald-400/40 border-2 border-green-300 rounded-md max-w-4/5">
-<h2 class="text-emerald-300 font-bold text-2xl mb-5 flex items-center gap-2"><Icon name="material-symbols-light:info-outline-rounded"/><slot mdc-unwrap="p" name="title"  /></h2>
+<section class="py-2 px-4 bg-emerald-400/40 border-2 border-green-300 rounded-md max-w-4/5">
+<h2 class="text-emerald-300 mb-4 align-middle"><Icon name="material-symbols-light:info-outline-rounded"/><slot mdc-unwrap="p" name="title"  /></h2>
 <div>
 	<slot />
 </div>
