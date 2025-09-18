3cacffd9d6dbaba33d25add01159d52f7b213673
Tran Tan Loc <locphuc016@gmail.com>
2025-09-18 09:13:15 +0700

    test max width

--- app/pages/[year]/[subject]/[slug].vue
+++ app/pages/[year]/[subject]/[slug].vue
@@ -47,7 +47,7 @@ useSeoMeta({
         <p class="italic text-gray mt-2">{{ useDateFormat(post.date,'ddd, DD MMM YYYY') }}</p>
     </div>
   <!-- Render the content post as Prose & Vue components -->
-   <article class="post-content prose-wrapper prose-headings:font-[Montserrat] prose-headings:underline-offset-8 md:prose-xl  dark:prose-invert mx-auto mb-14 dark:text-gray-100">
+   <article class="post-content prose-wrapper prose-headings:font-[Montserrat] prose-headings:underline-offset-8 md:prose-xl lg:prose-lg max-w-[75ch] xl:max-w-[65ch] dark:prose-invert mx-auto mb-14 dark:text-gray-100">
        <ContentRenderer v-if="post" :value="post" />
        <NotFound v-else></NotFound>
    </article>
